735934221591ad3e31b175367b6f3fe64fe49f30
Gumbx <gustavo.bonassa1@gmail.com>
2019-06-03 15:51:23 -0300

    CRUD ads

--- src/routes.js
+++ src/routes.js
@@ -4,14 +4,20 @@ const routes = express.Router();
 
 const authMiddleware = require('./app/middlewares/auth');
 
-const UserController = require('./app/controllers/UserController')
-const SessionController = require('./app/controllers/SessionController')
+const controllers = require('./app/controllers')
 
-routes.post('/users', UserController.store)
-routes.post('/sessions', SessionController.store)
+routes.post('/users', controllers.UserController.store)
+routes.post('/sessions', controllers.SessionController.store)
 
-routes.get('/teste', authMiddleware, (req, res) => res.json({
-    ok: true
-}))
+routes.use(authMiddleware) //a partir daqui precisa estar autenticado
+
+/*
+ * Ads
+ */
+routes.get('/ads', controllers.AdController.index)
+routes.get('/ads/:id', controllers.AdController.show)
+routes.post('/ads', controllers.AdController.store)
+routes.put('/ads:/id', controllers.AdController.update)
+routes.delete('/ads/:id', controllers.AdController.destroy)
 
 module.exports = routes;
